beb328502b40c1ccffd3c8e92ec10bdb80f48b74
hunght21 <doquanghung204@gmail.com>
2023-06-10 16:32:32 +0700

    Init commit

---
+++ src/main/java/com/ltdd/nhakhoaapi/NhakhoaapiApplication.java
@@ -0,0 +1,13 @@
+package com.ltdd.nhakhoaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhakhoaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhakhoaapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ltdd/nhakhoaapi/NhakhoaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltdd.nhakhoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhakhoaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
